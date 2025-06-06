f8d38f78eff8d780bf5202bdd5498612e0446e84
Son Nguyen <nguyenminhson20051129@gmail.com>
2025-06-05 21:36:59 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,10 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  while (true) {
-    std::cout << "$ ";
-    std::string input;
-    std::getline(std::cin, input);
-    if (input == "exit") {
-      break;
-    }
-    std::cout << input << ": command not found\n";
-  }
+  std::cout << "$ ";
+
+  std::string input;
+  std::getline(std::cin, input);
+  std::cout << input << ": command not found" << std::endl;
+  main();
 }
