895d7a812cd25c6c47f085de85a63979c3a373d1
Son Nguyen <nguyenminhson20051129@gmail.com>
2025-06-05 21:49:02 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   std::cout << "$ ";
-
   std::string input;
   std::getline(std::cin, input);
+
+  // Check if input starts with "exit"
+  if (input.substr(0, 4) == "exit")
+  {
+    // If there's a space after "exit", try to parse the status code
+    if (input.length() > 4 && input[4] == ' ')
+    {
+      try
+      {
+        int status = std::stoi(input.substr(5));
+        if (status >= 0 && status <= 255)
+        {
+          return status;
+        }
+      }
+      catch (...)
+      {
+        // If parsing fails, just exit with 0
+        return 0;
+      }
+    }
+  }
   std::cout << input << ": command not found" << std::endl;
   main();
 }
