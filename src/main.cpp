d3e5135cc2b81d6fa295468369f1a87e459adb4d
Son Nguyen <nguyenminhson20051129@gmail.com>
2025-06-05 22:32:41 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,60 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <unordered_set>
 
 int main()
 {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
-  std::cout << "$ ";
-  std::string input;
-  std::getline(std::cin, input);
-
-  // Check if input starts with "exit"
-  if (input.substr(0, 4) == "exit")
+  std::unordered_set<std::string> builtin_commands = {"echo", "type", "exit"};
+  while (true)
   {
-    // If there's a space after "exit", try to parse the status code
-    if (input.length() > 4 && input[4] == ' ')
+    std::cout << "$ ";
+    std::string input;
+    std::getline(std::cin, input);
+    // Check if input starts with "exit"
+    if (input.substr(0, 4) == "exit")
     {
-      try
+      // If there's a space after "exit", try to parse the status code
+      if (input.length() > 4 && input[4] == ' ')
       {
-        int status = std::stoi(input.substr(5));
-        if (status >= 0 && status <= 255)
+        try
         {
-          return status;
+          int status = std::stoi(input.substr(5));
+          if (status >= 0 && status <= 255)
+          {
+            return status;
+          }
+        }
+        catch (...)
+        {
+          // If parsing fails, just exit with 0
+          return 0;
         }
       }
-      catch (...)
+      return 0;
+    }
+    else if (input.substr(0, 5) == "echo ")
+    {
+      std::cout << input.substr(5) << std::endl;
+    }
+    else if (input.substr(0, 5) == "type ")
+    {
+      if (builtin_commands.find(input.substr(5)) != builtin_commands.end())
       {
-        // If parsing fails, just exit with 0
-        return 0;
+        std::cout << input.substr(5) << " is a shell builtin" << std::endl;
+      }
+      else
+      {
+        std::cout << input.substr(5) << ": not found" << std::endl;
       }
     }
+    else
+    {
+      std::cout << input << ": command not found" << std::endl;
+    }
   }
-  else if (input.substr(0, 5) == "echo ")
-  {
-    std::cout << input.substr(5) << std::endl;
-  }
-  else {
-    std::cout << input << ": command not found" << std::endl;
-  }
-  
-  main();
   return 0;
 }
